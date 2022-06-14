e9b59a5af948d198e008cf7f2de692ad793f1a85
vinodell <max.khamitov@mail.ru>
2022-06-14 16:01:19 +0300

    add webpack for dev/prod

---
+++ webpack.config.js
@@ -0,0 +1,4 @@
+module.exports =
+  process.env.NODE_ENV === "development"
+    ? require("./webpack.dev")
+    : require("./webpack.prod");
---
+++ webpack.dev.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const { resolve } = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+
+const { PORT, SOCKETS_IO_STATUS } = process.env;
+
+const config = {
+  mode: "development",
+  optimization: {
+    usedExports: true,
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    // server: 'https',
+    contentBase: resolve(__dirname, "dist"),
+    port: 8081, // client port
+    host: "localhost",
+    client: {
+      overlay: {
+        warnings: false,
+        errors: true,
+      },
+    },
+    historyApiFallback: true,
+    proxy: {
+      context: ["/api", "/ws"],
+      target: `http://localhost:${PORT || 8080}`, // server port
+      ws: SOCKETS_IO_STATUS,
+    },
+  },
+};
+
+module.exports = merge(common, config);
